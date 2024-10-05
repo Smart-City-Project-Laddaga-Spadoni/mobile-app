3bf557d1f1993f9046fc1967c1395dcd1e42dc09
MichLadd <95909039+MichLadd@users.noreply.github.com>
2024-10-06 00:15:32 +0200

    feat: WIP jwt + setup page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'comunication/mqtt_service.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +15,168 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LightBulbControl(),
+      home: StartPage(),
+    );
+  }
+}
+
+class StartPage extends StatefulWidget {
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  final TextEditingController _serverController = TextEditingController();
+  final FlutterSecureStorage _storage = FlutterSecureStorage();
+
+  @override
+  void initState() {
+    super.initState();
+    _checkServerUrl();
+  }
+
+  Future<void> _checkServerUrl() async {
+    String? serverUrl = await _storage.read(key: 'server_url');
+    if (serverUrl != null) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    }
+  }
+
+  Future<void> _saveServerUrl() async {
+    await _storage.write(key: 'server_url', value: _serverController.text);
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Enter Server Address'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _serverController,
+              decoration: InputDecoration(labelText: 'Server Address'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _saveServerUrl,
+              child: Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final FlutterSecureStorage _storage = FlutterSecureStorage();
+
+  Future<void> _login() async {
+    String? serverUrl = await _storage.read(key: 'server_url');
+    try {
+      final response = await http.post(
+        Uri.parse('$serverUrl/login'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          'username': _usernameController.text,
+          'password': _passwordController.text,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        await _storage.write(key: 'jwt', value: data['access_token']);
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LightBulbControl()),
+        );
+      } else {
+        // Gestisci l'errore
+        _showErrorDialog('Login failed: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      // Gestisci l'errore di connessione
+      _showErrorDialog('Connection error: $e');
+    }
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Error'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () async {
+              await _storage.delete(key: 'server_url');
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => StartPage()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _login,
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -24,34 +187,82 @@ class LightBulbControl extends StatefulWidget {
 }
 
 class _LightBulbControlState extends State<LightBulbControl> {
-  late MQTTService mqttService;
   bool isLightOn = false;
+  final String deviceId = 'your_device_id'; // Sostituisci con il tuo device ID
+  final FlutterSecureStorage _storage = FlutterSecureStorage();
 
   @override
   void initState() {
     super.initState();
-    mqttService = MQTTService('10.0.2.2', 'flutter_client', 1883); // Usa l'indirizzo IP corretto
-    mqttService.connect().then((_) {
-      mqttService.subscribeToTopic('lamp/status');
-      mqttService.setOnMessageReceived((message) {
-        setState(() {
-          isLightOn = (message == 'on');
-        });
-      });
-    });
+    _fetchDeviceStatus();
   }
 
-  void toggleLight() {
+  Future<void> _fetchDeviceStatus() async {
+    String? serverUrl = await _storage.read(key: 'server_url');
+    final token = await _storage.read(key: 'jwt');
+    try {
+      final response = await http.get(
+        Uri.parse('$serverUrl/device/$deviceId'),
+        headers: {'Authorization': 'Bearer $token'},
+      );
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          isLightOn = data['status']['is_on'];
+        });
+      } else {
+        // Gestisci l'errore
+        _showErrorDialog('Failed to load device status: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      // Gestisci l'errore di connessione
+      _showErrorDialog('Connection error: $e');
+    }
+  }
+
+  Future<void> _toggleLight() async {
     setState(() {
       isLightOn = !isLightOn;
     });
-    mqttService.publishMessage('lamp/status', isLightOn ? 'on' : 'off');
+    String? serverUrl = await _storage.read(key: 'server_url');
+    final token = await _storage.read(key: 'jwt');
+    try {
+      final response = await http.post(
+        Uri.parse('$serverUrl/device/$deviceId'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+        body: json.encode({'status': {'is_on': isLightOn}}),
+      );
+      if (response.statusCode != 200) {
+        // Gestisci l'errore
+        _showErrorDialog('Failed to update device status: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      // Gestisci l'errore di connessione
+      _showErrorDialog('Connection error: $e');
+    }
   }
 
-  @override
-  void dispose() {
-    mqttService.disconnect();
-    super.dispose();
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Error'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -59,6 +270,18 @@ class _LightBulbControlState extends State<LightBulbControl> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Light Bulb Control'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () async {
+              await _storage.delete(key: 'server_url');
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => StartPage()),
+              );
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Column(
@@ -70,7 +293,7 @@ class _LightBulbControlState extends State<LightBulbControl> {
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: toggleLight,
+              onPressed: _toggleLight,
               child: Text(isLightOn ? 'Turn OFF' : 'Turn ON'),
             ),
           ],
